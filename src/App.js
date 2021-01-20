9f90e074ffa699d54ebd6cd0fdabaf3230a6df9a
David Humphrey <david.andrew.humphrey@gmail.com>
2021-01-20 13:39:46 -0500

    Refactor to pass use state and pass entire user as prop

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
+import { useState } from 'react';
+
 import './App.css';
 import ProfileCard from './ProfileCard';
 
 function App() {
-  // {
-  //   "id": 1,
-  //   "email": "george.bluth@reqres.in",
-  //   "first_name": "George",
-  //   "last_name": "Bluth",
-  //   "avatar": "https://reqres.in/img/faces/1-image.jpg"
-  // }
+  const [user, setUser] = useState({
+    id: 1,
+    email: "george.bluth@reqres.in",
+    first_name: "George",
+    last_name: "Bluth",
+    avatar: "https://reqres.in/img/faces/1-image.jpg"
+  });
 
   return (
     <div className="App">
-      <ProfileCard id="1" email="george.bluth" name="George Bluth" avatarUrl="https://reqres.in/img/faces/1-image.jpg" />
+      <ProfileCard user={user} />
     </div>
   );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -3,7 +3,12 @@ import './ProfileCard.css';
 import Avatar from './Avatar';
 import UserInfo from './UserInfo';
 
-function ProfileCard({id, name, email, avatarUrl}) {
+function ProfileCard({ user }) {
+  const id = user.id;
+  const email = user.email;
+  const name = `${user.first_name} ${user.last_name}`;
+  const avatarUrl = user.avatar;
+
   return (
     <section id={`user-${id}`} className="profile-card">
       <Avatar imgUrl={avatarUrl} name={name} />
